22ba6de392b02db56e55d1b5755314299a2ef496
robbykurnia2 <robby.putra@tokopedia.com>
2023-12-15 07:08:49 +0700

    feat: add script

---
+++ download-private-youtube.sh
@@ -0,0 +1,2 @@
+yt-dlp  --cookies=cookies.txt https://www.youtube.com/watch?v=YcVCTXjV0uA&list=PL-CtdCApEFH-0i9dzMzLw6FKVrFWv3QvQ&index=6&pp=iAQB
+# To obtain the cookies.txt file, use a browser extension (e.g., Get cookies.txt LOCALLY) to gain access and watch videos.
