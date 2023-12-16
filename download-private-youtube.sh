6af938ecb01357e03d660c64e2a4d9b4af2c70d8
robbykurnia2 <robby.putra@tokopedia.com>
2023-12-16 13:30:11 +0700

    feat: update cookies dan script

--- download-private-youtube.sh
+++ download-private-youtube.sh
@@ -1,2 +1,2 @@
-yt-dlp  --cookies=cookies.txt https://www.youtube.com/watch?v=YcVCTXjV0uA&list=PL-CtdCApEFH-0i9dzMzLw6FKVrFWv3QvQ&index=6&pp=iAQB
+yt-dlp --cookies=cookies.txt -o "TUTORIAL GOLANG GOROUTINES BAHASA INDONESIA.%(ext)s" --merge-output-format mp4 https://www.youtube.com/watch?v=YcVCTXjV0uA&list=PL-CtdCApEFH-0i9dzMzLw6FKVrFWv3QvQ&index=6&pp=iAQB
 # To obtain the cookies.txt file, use a browser extension (e.g., Get cookies.txt LOCALLY) to gain access and watch videos.
